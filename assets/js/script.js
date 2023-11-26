8453157f23caf3f8c17e04ebfee79306c662ed4f
tomcoldham <>
2023-11-26 19:59:20 +0000

    added in other placeholder texts for other questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,5 +7,95 @@ const questions = [
             { text: "answer1", correct: true },
             { text: "answer1", correct: true },
         ]
+    },
+    {
+        question: "this is placeholder text for first question?",
+        choices: [
+            { text: "answer1", correct: true },
+            { text: "answer1", correct: true },
+            { text: "answer1", correct: true },
+            { text: "answer1", correct: true },
+        ]
+    },
+    {
+        question: "this is placeholder text for first question?",
+        choices: [
+            { text: "answer1", correct: true },
+            { text: "answer1", correct: true },
+            { text: "answer1", correct: true },
+            { text: "answer1", correct: true },
+        ]
+    },
+    {
+        question: "this is placeholder text for first question?",
+        choices: [
+            { text: "answer1", correct: true },
+            { text: "answer1", correct: true },
+            { text: "answer1", correct: true },
+            { text: "answer1", correct: true },
+        ]
+    },
+    {
+        question: "this is placeholder text for first question?",
+        choices: [
+            { text: "answer1", correct: true },
+            { text: "answer1", correct: true },
+            { text: "answer1", correct: true },
+            { text: "answer1", correct: true },
+        ]
+    },
+    {
+        question: "this is placeholder text for first question?",
+        choices: [
+            { text: "answer1", correct: true },
+            { text: "answer1", correct: true },
+            { text: "answer1", correct: true },
+            { text: "answer1", correct: true },
+        ]
+    },
+    {
+        question: "this is placeholder text for first question?",
+        choices: [
+            { text: "answer1", correct: true },
+            { text: "answer1", correct: true },
+            { text: "answer1", correct: true },
+            { text: "answer1", correct: true },
+        ]
+    },
+    {
+        question: "this is placeholder text for first question?",
+        choices: [
+            { text: "answer1", correct: true },
+            { text: "answer1", correct: true },
+            { text: "answer1", correct: true },
+            { text: "answer1", correct: true },
+        ]
+    },
+    {
+        question: "this is placeholder text for first question?",
+        choices: [
+            { text: "answer1", correct: true },
+            { text: "answer1", correct: true },
+            { text: "answer1", correct: true },
+            { text: "answer1", correct: true },
+        ]
+    },
+    {
+        question: "this is placeholder text for first question?",
+        choices: [
+            { text: "answer1", correct: true },
+            { text: "answer1", correct: true },
+            { text: "answer1", correct: true },
+            { text: "answer1", correct: true },
+        ]
+    },
+    {
+        question: "this is placeholder text for first question?",
+        choices: [
+            { text: "answer1", correct: true },
+            { text: "answer1", correct: true },
+            { text: "answer1", correct: true },
+            { text: "answer1", correct: true },
+        ]
     }
 ];
